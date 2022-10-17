114a8439042d77e327e2b825df0b49a63acf6f5c
Sophie Wigmore <swigmore@vmware.com>
2022-10-17 16:44:19 -0400

    add FPM reload test

--- integration/httpd_reload_test.go
+++ integration/httpd_reload_test.go
@@ -170,5 +170,63 @@ func testHttpdReload(t *testing.T, context spec.G, it spec.S) {
 				OnPort(8080).
 				WithEndpoint("/index.php?date"))
 		})
+
+		it("will reload FPM config files", func() {
+			var (
+				logs fmt.Stringer
+				err  error
+			)
+
+			image, logs, err = pack.WithNoColor().Build.
+				WithPullPolicy("never").
+				WithBuildpacks(
+					phpDistBuildpack,
+					phpFpmBuildpack,
+					httpdBuildpack,
+					phpHttpdBuildpack,
+					watchexecBuildpack,
+					buildpack,
+				).
+				WithEnv(map[string]string{
+					"BP_LOG_LEVEL":           "DEBUG",
+					"BP_PHP_SERVER":          "httpd",
+					"BP_LIVE_RELOAD_ENABLED": "true",
+				}).
+				Execute(name, source)
+			Expect(err).ToNot(HaveOccurred(), logs.String)
+
+			Expect(logs).To(ContainLines(
+				"  Determining start commands to include in procs.yml:",
+				MatchRegexp(`    HTTPD: watchexec --watch /workspace/\.httpd\.conf\.d --on-busy-update signal --signal SIGHUP --shell none -- httpd -f /layers/.*/php-httpd-config/httpd\.conf -k start -DFOREGROUND`),
+				MatchRegexp(`    FPM: watchexec --watch /workspace/\.php\.fpm\.d --on-busy-update signal --signal SIGUSR2 --shell none -- php-fpm -y /layers/.*/php-fpm-config/base.conf -c /layers/.*/php/etc`),
+			))
+
+			Expect(logs).To(ContainLines(
+				"  Assigning launch processes:",
+				fmt.Sprintf("    web (default): procmgr-binary /layers/%s/php-start/procs.yml", strings.ReplaceAll(buildpackInfo.Buildpack.ID, "/", "_")),
+			))
+
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				WithPublishAll().
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(Serve(ContainSubstring("SUCCESS: date loads.")).OnPort(8080).WithEndpoint("/index.php?date"))
+
+			err = docker.Container.Exec.ExecuteBash(container.ID, "sed -i 's/pm.max_children = 5/pm.max_children = 6/g' /workspace/.php.fpm.d/user.conf")
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() string {
+				cLogs, err := docker.Container.Logs.Execute(container.ID)
+				Expect(err).NotTo(HaveOccurred())
+				return cLogs.String()
+			}).Should(And(
+				ContainSubstring(`Reloading in progress ...`),
+				ContainSubstring(`reloading: execvp("php-fpm", {"php-fpm", "-y"`),
+			))
+			Eventually(container).Should(Serve(ContainSubstring("SUCCESS: date loads.")).OnPort(8080).WithEndpoint("/index.php?date"))
+		})
 	})
 }
